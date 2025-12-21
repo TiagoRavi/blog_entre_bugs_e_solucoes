49d832efe146870b8303febfb4d1124c50d7fd4a
Tiago <tiagodesousacosta2024@gmail.com>
2025-12-21 15:51:08 -0300

    Deploy-ready: Django blog with Render config, Postgres, admin automation

---
+++ blog/management/commands/create_admin.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    help = "Cria superusuário automaticamente se não existir"
+
+    def handle(self, *args, **options):
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+        if not username or not password:
+            self.stdout.write("Superuser vars não definidas")
+            return
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write("Superuser já existe")
+            return
+
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+        self.stdout.write("Superuser criado com sucesso")
