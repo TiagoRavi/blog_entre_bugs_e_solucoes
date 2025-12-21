e16fc6360cd44ad6346b522dbbe7f5a9a2081b8d
Tiago <tiagodesousacosta2024@gmail.com>
2025-12-21 13:36:05 -0300

    mudei .env v1

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -14,38 +14,38 @@ ALLOWED_HOSTS = os.environ.get(
 # SECURITY
 # ======================================================
 
-# Força HTTPS
 SECURE_SSL_REDIRECT = True
 
-# Cookies seguros (somente HTTPS)
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-# HSTS — habilitar SOMENTE quando HTTPS estiver OK
-SECURE_HSTS_SECONDS = 31536000  # 1 ano
+SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
-# Headers de segurança
-SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
-
 X_FRAME_OPTIONS = "DENY"
 
-# CSRF Trusted Origins (vem do .env)
 CSRF_TRUSTED_ORIGINS = os.environ.get(
     "DJANGO_CSRF_TRUSTED_ORIGINS", ""
 ).split(",")
 
 # ======================================================
-# CACHE (PROD)
+# CACHE
 # ======================================================
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": os.environ.get(
-            "REDIS_URL",
-            "redis://127.0.0.1:6379/1",
-        ),
+REDIS_URL = os.environ.get("REDIS_URL")
+
+if REDIS_URL:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": REDIS_URL,
+        }
     }
-}
+else:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+            "LOCATION": "render-fallback-cache",
+        }
+    }
