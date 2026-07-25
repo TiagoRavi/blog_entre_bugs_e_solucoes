13690ac8196b1df9f9e81c4c1c09f3ea42180be5
Tiago <tiagodesousacosta2024@gmail.com>
2026-07-25 10:18:27 -0300

    fix(cloudinary): force secure URLs in production

--- blog/apps.py
+++ blog/apps.py
@@ -1,8 +1,19 @@
 from django.apps import AppConfig
+from django.conf import settings
+import cloudinary
+
 
 class BlogConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "blog"
+
+    def ready(self):
+        cloudinary.config(
+            cloud_name=settings.CLOUDINARY_STORAGE["CLOUD_NAME"],
+            api_key=settings.CLOUDINARY_STORAGE["API_KEY"],
+            api_secret=settings.CLOUDINARY_STORAGE["API_SECRET"],
+            secure=True,
+        )
 
     def ready(self):
         import blog.signals  # noqa
